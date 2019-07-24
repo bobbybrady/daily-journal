2bdb397aea2766646f29349f939b172f1fe236ac
Bobby Brady <bobbybrady@Bobbys-MacBook-Pro-2.local>
2019-07-24 14:33:43 -0500

    added api

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -2,34 +2,33 @@
     Define the keys and value for a JavaScript object that
     represents a journal entry about what you learned today
 */
-const journalEntry = {
-    date : '07/10/2019',
-    concepts : 'flexbox',
-    journal : 'Today I learned flexbox, by playing the frog game.',
-    mood: 'Happy'
-}  
+// const journalEntries = [
+//     {
+//         date : '07/10/2019',
+//         concepts : 'flexbox',
+//         journal : 'Today I learned flexbox, by playing the frog game.',
+//         mood: 'Happy'
+//     },  
 
-const journalEntryOne = {
-    date : '07/11/2019',
-    concepts : 'git',
-    journal : 'Today I practiced git and pushed all of my repositories to GitHub.',
-    mood: 'content'
-}  
+//     {
+//         date : '07/11/2019',
+//         concepts : 'git',
+//         journal : 'Today I practiced git and pushed all of my repositories to GitHub.',
+//         mood: 'content'
+//     },  
 
-const journalEntryTwo = {
-    date : '07/12/2019',
-    concepts : 'Javascript Objects' ,
-    journal : 'Practiced creating objects as well as targeting specfic',
-    mood: 'happy'
-}  
+//     {
+//         date : '07/12/2019',
+//         concepts : 'Javascript Objects' ,
+//         journal : 'Practiced creating objects as well as targeting specfic',
+//         mood: 'happy'
+//     }  
+// ]
 
-const journalEntries = []
 
-journalEntries.push(journalEntry)
-journalEntries.push(journalEntryOne)
-journalEntries.push(journalEntryTwo)
 
-console.log(journalEntries)
+
+// console.log(journalEntries)
 const toHTML = document.querySelector("#entries")
 const makeJournalEntryComponent = (object) => {
     return `
@@ -43,7 +42,6 @@ const makeJournalEntryComponent = (object) => {
     </article>`
 
 }
-// const journal = makeJournalEntryComponent()
 
 let allEntries = ""
 let fragment = document.createDocumentFragment()
@@ -56,16 +54,18 @@ const renderJournalEntries = (entries) => {
     toHTML.innerHTML = allEntries
 }
 
-renderJournalEntries(journalEntries)
+// renderJournalEntries(journalEntries)
 
-// const createSection1 = (array) => {
-//     array.forEach(message => {
-//         const section = document.createElement('section')
-//         section.className = "message"
-//         section.textContent = message
-//         fragment.appendChild(section)
-        
-//     });
-//     toHTML.appendChild(fragment)
-// }
+const getData = () => {
+    fetch("http://localhost:3000/entries")
+        .then(entries => entries.json())
+        .then(entries => {
+            for (const entry of entries) {
+                const convertedEntry = makeJournalEntryComponent(entry)
+                toHTML.innerHTML += convertedEntry
+            }
+        })
+}
+
+getData()
 
